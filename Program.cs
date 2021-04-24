cc049e6510597fea9d290bd4e71fcf1df22f9d9f
Horg <horgovi4@gmail.com>
2021-04-24 12:40:00 +0300

    add exercise

--- Program.cs
+++ Program.cs
@@ -6,53 +6,72 @@ using System.Threading.Tasks;
 using System.IO;
 
 
-namespace exercise4
+namespace test
 {
-  
+    class Massive
+    {
+        // 
+        // Почему я не могу внести значение переменной b в параметр int[b] когда создаю массив,
+        // для того, что б указать размерность массива
+        static int b;
+        int start;
+        int step;
+        public Massive(int max, int start, int step)
+        {
+            b = max;
+            this.start = start;
+            this.step = step;
+
+        }
+        public int[] a = new int[b];
+       
+
+        public void Fill()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = start;
+                start += step;
+                Console.Write($"{a[i]} ");
+            }
+            Console.WriteLine("");
+        }
+
+            
+            
+           
+        public void Inverse()
+        {
+            int[] beta = new int[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                beta[i] = -a[i];
+                Console.Write($"{beta[i]} ");
+            }
+            Console.WriteLine("");
+        }
+        public void Multi()
+        {
+            Console.WriteLine("На какое число хотите умножить каждый элемент массива?: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = a[i] * n;
+                Console.Write($"{a[i]} ");
+            }
+            Console.WriteLine("");
+        }
+
+    }
+
+
     class Program
     {
-        public static string[] loginMain = new string[5];
-        public static string[] passwordMain = new string[5];
-        public static bool LogPass(string log)//, string pass)
-        {
-            int i = 0;
-            while (log != loginMain[i])
-            {
-                Console.WriteLine("Введите логин: ");
-                log = Console.ReadLine();
-                i++;
-            }
-            while ()
-            {
 
-            }
-
-            return true;
-        }
-
-
-        public static void info(string[] login, string[] password)
-        {
-            StreamReader logins = new StreamReader("login.txt");
-            StreamReader passwords = new StreamReader("password.txt");
-
-            for (int i = 0; i < login.Length; i++)
-            {
-                login[i] = logins.ReadLine();
-            }
-            Console.WriteLine("brrr");
-            for (int i = 0; i < password.Length; i++)
-            {
-                password[i] = passwords.ReadLine();
-            }
-        }
         static void Main(string[] args)
         {
-            info(loginMain, passwordMain);
-            string a = Console.ReadLine();
-            if (LogPass(a) == true) Console.WriteLine("yyyees");
-            else Console.WriteLine("nooopee");
-
+            Massive array = new Massive(10, 1, 2);
+            array.Fill();
 
         }
     }
