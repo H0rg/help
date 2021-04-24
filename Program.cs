b263420e72e73d3f6fd5c7d06ed02e0608c899fa
Horg <horgovi4@gmail.com>
2021-04-24 12:38:52 +0300

    add exercise

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace exercise4
+{
+  
+    class Program
+    {
+        public static string[] loginMain = new string[5];
+        public static string[] passwordMain = new string[5];
+        public static bool LogPass(string log)//, string pass)
+        {
+            int i = 0;
+            while (log != loginMain[i])
+            {
+                Console.WriteLine("Введите логин: ");
+                log = Console.ReadLine();
+                i++;
+            }
+            while ()
+            {
+
+            }
+
+            return true;
+        }
+
+
+        public static void info(string[] login, string[] password)
+        {
+            StreamReader logins = new StreamReader("login.txt");
+            StreamReader passwords = new StreamReader("password.txt");
+
+            for (int i = 0; i < login.Length; i++)
+            {
+                login[i] = logins.ReadLine();
+            }
+            Console.WriteLine("brrr");
+            for (int i = 0; i < password.Length; i++)
+            {
+                password[i] = passwords.ReadLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            info(loginMain, passwordMain);
+            string a = Console.ReadLine();
+            if (LogPass(a) == true) Console.WriteLine("yyyees");
+            else Console.WriteLine("nooopee");
+
+
+        }
+    }
+}
